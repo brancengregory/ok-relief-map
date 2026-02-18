8334a6d22c6e816e6ebd3929d90229459b2efc5e
Brancen Gregory <brancengregory@gmail.com>
2026-02-17 19:56:38 -0600

    feat: add basic shader

--- R/explore.R
+++ R/explore.R
@@ -1,60 +1,28 @@
-library(elevatr)
-library(raster)
-library(sf)
 library(rayshader)
-library(crsuggest)
-library(rgdal)
 library(tigris)
 library(dplyr)
-library(magick)
-
-# coords <- data.frame(
-#   x = c(33.615833, 37.002206),
-#   y = c(-103.002565, -94.430662)
-# )
-#
-# elev <- coords |>
-#   st_as_sf(coords = c("x", "y"), crs = 2267) |>
-#   get_elev_raster(z = 12)
-#
-# elev_mat <- raster_to_matrix(elev)
-#
-# elev_mat |>
-#   sphere_shade() |>
-#   plot_map()
+library(elevatr)
 
 # Oklahoma shapefile ----
 ok_shape <- states(cb = F) |>
   filter(NAME == "Oklahoma")
 
 elev_ok <- ok_shape |>
-  get_elev_raster(z = 8)
+  get_elev_raster(
+    z = 8,
+    clip = "locations"
+  )
 
-elev_ok_shaped <- crop(
-  elev_ok,
-  extent(ok_shape)
-)
+ok_mat <- raster_to_matrix(elev_ok)
 
-elev_ok_shaped <- mask(
-  elev_ok_shaped,
-  ok_shape
-)
-
-elev_ok_mat <- raster_to_matrix(elev_ok_shaped)
-
-# elev_ok_mat |>
-#   sphere_shade() |>
-#   plot_map()
-
-elev_ok_mat |>
-  sphere_shade(texture = "bw", zscale = 8) |>
+ok_map <- ok_mat |>
+  sphere_shade() |>
   add_water(
-    detect_water(elev_ok_mat)
+    detect_water(ok_mat)
   ) |>
-  #add_shadow(ray_shade(elev_ok_mat)) |>
-  plot_map(title_text = "Oklahoma")
-  # plot_3d(heightmap = elev_ok_mat,
-  #         solid = TRUE, shadow = FALSE)
+  add_shadow(ray_shade(ok_mat,lambert=FALSE)) |>
+  add_shadow(lamb_shade(ok_mat)) |>
+  add_shadow(ambient_shade(ok_mat))
 
-
-
+ok_map |>
+  plot_map()
